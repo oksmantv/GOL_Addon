e7e958e94ed135ad5ccb2a35437a966d493350c8
Alex Carnegie <62963173+Bluwolf00@users.noreply.github.com>
2025-08-13 17:32:23 +0100

    Add A-10 and increase volume

--- addons/lockalerts/CfgVehicles.cpp
+++ addons/lockalerts/CfgVehicles.cpp
@@ -1,10 +1,19 @@
 class CfgVehicles {
     class Plane_Fighter_01_Base_F;
-
     class B_Plane_Fighter_01_F: Plane_Fighter_01_Base_F {
-        soundIncommingMissile[] = {"\x\gw\addons\lockalerts\sounds\missile.wss", 1, 1};
-        soundLocked[] = {"\x\gw\addons\lockalerts\sounds\incoming.wss", 1, 1};
-    }
+        soundIncommingMissile[] = {"x\gw\addons\lockalerts\Data\sounds\F18_missile.wss", 2.8, 1};
+        soundLocked[] = {"x\gw\addons\lockalerts\Data\sounds\F18_incoming.wss", 2.8, 1};
+        
+    };
+    class B_Plane_Fighter_01_Stealth_F: Plane_Fighter_01_Base_F {
+        soundIncommingMissile[] = {"x\gw\addons\lockalerts\Data\sounds\F18_missile.wss", 2.8, 1};
+        soundLocked[] = {"x\gw\addons\lockalerts\Data\sounds\F18_incoming.wss", 2.8, 1};
+    };
 
+    class Plane;
+    class USAF_A10: Plane {
+        soundIncommingMissile[] = {"x\gw\addons\lockalerts\Data\sounds\A10_missile.wss", 2.5, 1};
+        soundLocked[] = {"x\gw\addons\lockalerts\Data\sounds\A10_incoming.wss", 2, 1};
+    };
     
 };
